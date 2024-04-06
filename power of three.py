0c3e3e61345d949d81b37fd2845ba0b086176f78
MatamChandrika26 <163716277+MatamChandrika26@users.noreply.github.com>
2024-04-06 12:19:02 +0530

    Create power of three.py

---
+++ power of three.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        def pow2(m):
+            if m==1:
+                return True
+            elif m<1 or m%3!=0:
+                return False
+            else:
+                return pow2(m/3)
+        return n>0 and pow2(n)
